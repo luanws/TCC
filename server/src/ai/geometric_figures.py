72b30d5b5fc525ded07fbb378232992bd846fb78
Luan Willig Silveira <luan.w.silveira@gmail.com>
2023-04-16 14:55:48 -0300

    server: predição com pré-processamento

--- server/src/ai/geometric_figures.py
+++ server/src/ai/geometric_figures.py
@@ -2,7 +2,7 @@ import keras
 import numpy as np
 from PIL import Image
 
-from src.utils import files
+from src.utils import files, preprocess
 
 
 class GeometricFigureClassifier:
@@ -10,7 +10,7 @@ class GeometricFigureClassifier:
     h5_file_url: str = 'https://download847.mediafire.com/9dsd959zgvsgBdchwIixXUWh8dRl6vaTEPKa0Ru-V8L6bGYwSoc3OrMe68GGLQE0UPJHpwv1_UmGnwItot_hud8FLfk/y9wi7sq5cgzyycl/geometric_figure_classifier.h5'
     filename: str = 'data/geometric_figure_classifier.h5'
     categories: list[str] = ['circle', 'square', 'triangle']
-    image_size: int = 512
+    image_size: int = 128
 
     def __new__(cls):
         if not hasattr(cls, 'instance'):
@@ -39,15 +39,28 @@ class GeometricFigureClassifier:
     def __decode_prediction(self, prediction: np.ndarray) -> str:
         return self.categories[np.argmax(prediction)]
     
+    def preprocess_input(self, x: np.ndarray) -> np.ndarray:
+        x = x.copy()
+        x = x[:, :, 1:2]
+        x = preprocess.normalize(x)
+        x = x**10
+        x = preprocess.normalize(x)
+        x = np.heaviside(x - 0.15, 1)
+        x_size = x.shape[0] * x.shape[1]
+        min_size_remove_stain = 3000*(x_size)/(512**2)
+        x = preprocess.remove_stain(x, min_size_remove_stain)
+        x = preprocess.normalize(x)
+        return x
+
     def __preprocess_image(self, image: Image) -> np.ndarray:
         image = image.resize((self.image_size, self.image_size))
         image_array = np.array(image, dtype=np.float32) / 255
-        image_array = image_array[:, :, 1]
+        image_array = self.preprocess_input(image_array)
         image_array = np.expand_dims(image_array, axis=0)
         return image_array
 
     def predict(self, image: Image) -> str: 
         image = self.__preprocess_image(image)
-        prediction = self.model.predict(image)
+        prediction = self.model.predict([image])
         return self.__decode_prediction(prediction)
         
---
+++ server/src/utils/preprocess.py
@@ -0,0 +1,26 @@
+import numpy as np
+from scipy.ndimage import label
+
+
+def normalize(image: np.ndarray) -> np.ndarray:
+    image = image - np.min(image)
+    max_value = np.max(image)
+    if max_value != 0:
+        image = image / max_value
+    return image
+
+
+def remove_stain(image: np.ndarray, min_size: int) -> np.ndarray:
+    # Rotula as regiões conectadas de pixels brancos
+    labels, number_of_labels = label(image)
+    # Conta o número de pixels brancos em cada região
+    count = np.bincount(labels.flatten())[1:]
+    # Elimina as regiões com poucos pixels brancos
+    mask = np.zeros_like(labels, dtype=np.bool)
+    for i in range(1, number_of_labels+1):
+        if count[i-1] >= min_size:
+            mask |= labels == i
+    # Aplica a máscara para remover as manchas
+    image_without_stain = np.zeros_like(image)
+    image_without_stain[mask] = image[mask]
+    return image_without_stain
